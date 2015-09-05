86028349bbeb0d72ffa1c2c24264826bb94b2dac
John  Touba <john.touba@gmail.com>
2015-09-05 21:17:13 +0200

    Add gun colider

--- Assets/Scripts/chief.cs
+++ Assets/Scripts/chief.cs
@@ -14,23 +14,35 @@ public class chief : InputDonjon {
 	public int fireGunCoolingSpeed;
 	private bool gunUse ;
 	private int _maxFireGun;
+	private float firgunSize;
+
 
 	// Use this for initialization
 	void Start () {
 		_maxFireGun = 0;
 		gunUse = true;
+		firgunSize = 0;
+
+
 
 
 	}	
 	
 	// Update is called once per frame
 	void Update () {
+		BoxCollider b = firegun.collider as BoxCollider;
 		var inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices [playerNum] : null;
 		updateWithInControl (inputDevice);
 		if (inputDevice.Action1 && gunUse == true) {
+			firgunSize += 4f * Time.deltaTime;
+			if (firgunSize <3.3){
+				b.size = new Vector3(1.0f,firgunSize,0.7f);
+				b.center = new Vector3(0.0f,(0.0f+firgunSize)/2,0.0f);
+			}
 			firegun.SetActive (true);
 			_maxFireGun += fireGunOverheatingSpeed;				
 		} else if (! inputDevice.Action1 || gunUse == false) {
+			firgunSize = 0.0f;
 			firegun.SetActive (false);
 			if (_maxFireGun > 0)
 				_maxFireGun -= fireGunCoolingSpeed;
