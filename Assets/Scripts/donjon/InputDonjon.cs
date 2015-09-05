761f256ea2da92f17815e18822535afc6939a72b
John  Touba <john.touba@gmail.com>
2015-09-05 16:53:20 +0200

    Add chief script for firegun

--- Assets/Scripts/donjon/InputDonjon.cs
+++ Assets/Scripts/donjon/InputDonjon.cs
@@ -11,48 +11,37 @@ public class InputDonjon : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+	public void updateWithInControl(InputDevice inputDevice){
+				//var inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+				float moveY = 0;
+				float moveX = 0;
+				moveX -= inputDevice.LeftStickX;
+				if (moveX == 0)
+						moveX -= inputDevice.DPad.X;
+				moveY -= inputDevice.LeftStickY;
+				if (moveY == 0)
+						moveY -= inputDevice.DPad.Y;
+				if (inputDevice.LeftStick) {
+						Debug.Log ("debuSTICK");
+						transform.position += new Vector3 (-moveX * speed, 0, -moveY * speed);
+			
+						if (inputDevice != null) {
+								if (Mathf.Abs (inputDevice.LeftStickX) >= 0.19 || Mathf.Abs (inputDevice.LeftStickY) >= 0.19) {
+										transform.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Atan2 (inputDevice.LeftStickX, inputDevice.LeftStickY) * Mathf.Rad2Deg, 0));		
+										if (transform.rotation.eulerAngles.y != 180 && transform.rotation.eulerAngles.y != -180)
+												saveRotate = transform.rotation;
+								} else {
+										transform.rotation = saveRotate;	
+								}
+						} else {
+								transform.rotation = saveRotate;	
+						}
+				}
+		}
 	// Update is called once per frame
 	void Update () {
-		var inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
-
-		float moveY = 0;
-		float moveX = 0;
-		moveX -= inputDevice.LeftStickX;
-		if(moveX == 0)
-			moveX -= inputDevice.DPad.X;
-		moveY -= inputDevice.LeftStickY;
-		if(moveY==0)
-			moveY -= inputDevice.DPad.Y;
-		if (inputDevice.LeftStick) 
-		{
-			Debug.Log ("debuSTICK");
-			transform.position += new Vector3 (-moveX*speed, 0, -moveY*speed);
-
-			if (inputDevice != null ) 
-			{
-				if (Mathf.Abs (inputDevice.LeftStickX) >= 0.19 || Mathf.Abs (inputDevice.LeftStickY) >= 0.19 ) {
-					transform.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Atan2 (inputDevice.LeftStickX, inputDevice.LeftStickY) * Mathf.Rad2Deg, 0));		
-					if (transform.rotation.eulerAngles.y != 180 && transform.rotation.eulerAngles.y != -180)
-						saveRotate = transform.rotation;
-				}
-				else 
-				{
-					transform.rotation = saveRotate;	
-				}
-			}
-			else 
-			{
-				transform.rotation = saveRotate;	
-			}
-		}
-
-
-
-
-
-
-
+			var inputDevice = (InputManager.Devices.Count > playerNum) ? InputManager.Devices[playerNum] : null;
+			this.updateWithInControl(inputDevice);
 
 	}
 }
